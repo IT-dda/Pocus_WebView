a26f2ffecd732a8b1bab0b7dea4291d2a3929bf8
itda1 <tjdus2577@naver.com>
2022-05-05 16:11:40 +0900

    Refactor: Change 프로젝트 구조 재설정

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
-// express 가져오기
+'use strict';
+
+// 모듈
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-const port = 8000;
 
-app.set(port);
+// 라우팅
+const home = require('./src/routes/home');
+
+// 앱 세팅
+app.set('views', './src/views');
 app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
-app.use(express.static('static'));
+// use: 미들웨어 등록 메서드
+app.use(express.static(`${__dirname}/src/public`)); // __dirname: app.js 위치 반환
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); // url을 통해 전달되는 데이터에 한글, 공백 등의 문자가 포함될 경우 제대로 인식되지 않는 문제 해결
 
-app.get('/', (req, res) => {
-    res.render('index', { parameter1: 5, parameter2: '얍' });
-});
+app.use('/', home);
 
-app.listen(port, () => {
-    console.log('8000!');
-});
+module.exports = app;
