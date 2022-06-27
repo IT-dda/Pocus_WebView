b8ee2852ee1b1fb37eb9a4e0ea2cb2df1b757914
jeong-yeji <jeongyeji.mail@gmail.com>
2022-06-27 16:20:12 +0900

    Feat: MP, SS 모델 추가

---
+++ src/models/SS.js
@@ -0,0 +1,12 @@
+class SS {
+  constructor(ss_id, user_id, date, ss1, ss2, ss3, ss4) {
+    this.ss_id = ss_id;
+    this.user_id = user_id;
+    this.date = date;
+    this.ss1 = ss1;
+    this.ss2 = ss2;
+    this.ss3 = ss3;
+    this.ss4 = ss4;
+  }
+}
+module.exports = SS;
