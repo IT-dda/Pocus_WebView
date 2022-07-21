f586adfb89d4e22a22f6b732e279d312df249dd1
jeong-yeji <jeongyeji.mail@gmail.com>
2022-07-21 15:04:47 +0900

    Feat: DML 수정

--- src/sql/pocus_dml.sql
+++ src/sql/pocus_dml.sql
@@ -4,6 +4,7 @@ select * from user;
 
 insert into log (user_id, warning, isUpper) values (0, '거북목 자세가 감지되었습니다.', 1);
 insert into log (user_id, warning, isUpper) values (1, '하체 자세의 무게중심이 흐트러졌습니다.', 0);
+select * from log;
 
 insert into ss (user_id, ss1, ss2, ss3, ss4) values (12, -1.123456789, -0.123456789, 0.123456789, 1.123456789);
 insert into ss (user_id, ss1, ss2, ss3, ss4) values (12, -1.123456789, -0.123456789, 0.123456789, 1.123456789);
