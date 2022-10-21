270151e506a347dce6a1e4db1bba586e19695da8
seoyeonko <seoyeon99o@naver.com>
2022-10-22 02:22:34 +0900

    feat: main 페이지 소개 글 추가

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -9,7 +9,60 @@ document.addEventListener('DOMContentLoaded', function () {
     .go();
 });
 
+function renderComments() {
+  fetch('/data/comments.json')
+    .then((res) => res.json())
+    .then((data) => {
+      document.querySelector('.comment').innerHTML = `
+      <h1>${data.tit}</h1>
+      <ul class="comment-list">
+        <li>
+          <h2>${data.articles[0].subtit} 💻</h2>
+          <p>${data.articles[0].desc}</p>
+        </li>
+        <li>
+          <h2>${data.articles[1].subtit} 📝</h2>
+          <p>${data.articles[1].desc}</p>
+        </li>
+        <li>
+          <h2>${data.articles[2].subtit} 🙆🏻</h2>
+          <p>${data.articles[2].desc}</p>
+        </li>
+      </ul>
+      `;
+    });
+}
+
+function renderRecommand() {
+  fetch('/data/recommand.json')
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      document.querySelector('.recommand').innerHTML = `
+      <h1>${data.tit}</h1>
+      <ul class="recommand-list">
+        <li>
+          <img src="/image/${data.articles[0].img}">
+          <p>${data.articles[0].desc}</p>
+        </li>
+        <li>
+          <img src="/image/${data.articles[1].img}">
+          <p>${data.articles[1].desc}</p>
+        </li>
+        <li>
+          <img src="/image/${data.articles[2].img}">
+          <p>${data.articles[2].desc}</p>
+        </li>
+      </ul>
+      `;
+    });
+}
+
 // TODO:
 // - [ ] 로그인 확인 -> /init
 // - [ ] 로그인 미확인 -> /login
 $btn_get_start.addEventListener('click', () => (location.href = '/init'));
+document.addEventListener('DOMContentLoaded', () => {
+  renderComments();
+  renderRecommand();
+});
