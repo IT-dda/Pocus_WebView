f4081a50c266371b81cbf604c594c98e83f06855
99sujeong <ansdbs146@gmail.com>
2022-11-01 17:24:41 +0900

    ddl, dml

--- src/sql/pocus_dml.sql
+++ src/sql/pocus_dml.sql
@@ -2,16 +2,9 @@ insert into user (id, password, nickname, birthday, gender, height, weight) valu
 insert into user(id, password, birthday, gender, height, weight) values ('efgh', '1111', '1999-12-31', 1, 222, 22);
 select * from user;
 
-insert into log (user_id, warning, isUpper) values (0, '거북목 자세가 감지되었습니다.', 1);
-insert into log (user_id, warning, isUpper) values (1, '하체 자세의 무게중심이 흐트러졌습니다.', 0);
+insert into log (user_id, warning, isUpper) values (12, '거북목 자세가 감지되었습니다.', 1);
+insert into log (user_id, warning, isUpper) values (12, '하체 자세의 무게중심이 흐트러졌습니다.', 0);
 select * from log;
 
-insert into ss (user_id, ss1, ss2, ss3, ss4) values (12, -1.123456789, -0.123456789, 0.123456789, 1.123456789);
-insert into ss (user_id, ss1, ss2, ss3, ss4) values (12, -1.123456789, -0.123456789, 0.123456789, 1.123456789);
-insert into ss (user_id, ss1, ss2, ss3, ss4) values (12, -1.123456789, -0.123456789, 0.123456789, 1.123456789);
-insert into ss (user_id, ss1, ss2, ss3, ss4) values (12, -1.123456789, -0.123456789, 0.123456789, 1.123456789);
+insert into ss (log_id, ss1, ss2, ss3, ss4) values (4, 111, 222, 333, 444);
 select * from ss;
-
-insert into initss (user_id, ss_id) values (1, 1);
-insert into initss (user_id, ss_id) values (2, 2);
-select * from initss;
